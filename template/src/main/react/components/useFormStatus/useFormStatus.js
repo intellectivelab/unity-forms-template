c4763b4b73ef97958153ba52edd1e2f0f2967c05
Ilya Chukanov <ichukanov@intellective.com>
2021-04-12 14:11:09 +0300

    2021.1.0-alpha.1

--- template/src/main/react/components/useFormStatus/useFormStatus.js
+++
@@ -1,9 +0,0 @@
-import * as R from "ramda";
-
-export default (data) => {
-	const status = R.pathOr(R.path(["EP_Status", "value"], data), ["EP_Status", "value", "value"], data);
-
-	const isDraft = R.equals("Draft", status);
-
-	return {status, isDraft};
-};
